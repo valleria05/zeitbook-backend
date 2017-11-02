9c401f0d0d8c54f08721940322c6deb1992c502f
Hok Laam Cheng <hoklaam.cheng@gmail.com>
2017-11-02 13:18:47 -0400

    Set up Express

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var admin = require("firebase-admin");
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var serviceAccount = require("./serviceAccountKey.json");
+
+// Express
+var app = express();
+var router = express.Router();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
