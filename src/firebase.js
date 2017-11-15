5c15a1fc6bd08e80afe3a70afce820bfca7baa95
Fabio Nobre <fabio@zeitspace.com>
2017-11-15 16:29:35 -0500

    Fix private key from enviroment

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,12 @@
 const serviceAccount = process.env.NODE_ENV === 'production' ? getServiceAccountFromEnv() : require('../serviceAccountKey.json');
 
 function getServiceAccountFromEnv() {
+    const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
     return {
         type: 'service_account',
         project_id: process.env.PROJECT_ID,
         private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATE_KEY,
+        private_key: privateKey,
         client_email: process.env.CLIENT_EMAIL,
         client_id: process.env.CLIENT_ID,
         auth_uri: process.env.AUTH_URI,
